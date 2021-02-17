72b84bd42257592a4d4fdb63b4451df17cb75cbc
Saurabh <saurabh.dilse@gmail.com>
2021-02-17 20:40:07 +0530

    Updated README.md file

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,8 +9,12 @@ def setup(browser):
     global driver
     if browser == 'chrome':
         if platform == "win32":
-            driver = webdriver.Chrome(executable_path="." + os.sep + "browsers" + os.sep + "chromedriver.exe")
-            print("Launching chrome browser in Windows.........")
+            try:
+                driver = webdriver.Chrome(executable_path="." + os.sep + "browsers" + os.sep + "chromedriver.exe")
+                print("Launching chrome browser in Windows.........")
+            except:
+                driver = webdriver.Chrome()
+                print("Launching chrome browser in Windows.........")
         elif platform == "darwin":
             driver = webdriver.Chrome()
             print("Launching chrome driver in Mac.....")
